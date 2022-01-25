ff05fed76de95c1c1defa2a2d6e7b2fbac911a89
Dan Payne <dpayne5532@gmail.com>
2022-01-25 16:56:53 -0700

    Initial Commit

---
+++ MoonShot2/MoonShot2App.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShot2App.swift
+//  MoonShot2
+//
+//  Created by Dan Payne on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonShot2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
