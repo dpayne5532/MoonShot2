7c56546c40346738be2c71bbcedec9baec329db4
Dan Payne <dpayne5532@gmail.com>
2022-01-29 04:59:03 -0700

    Complete Moonshot

---
+++ MoonShot2/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot2
+//
+//  Created by Dan Payne on 1/25/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
